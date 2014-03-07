ab912d6960152cedb146cec6f32dd2d5022f77b1
bitterguo <bitterguo@foxmail.com>
2014-03-07 14:05:12 +0800

    Now can display message in panel_detail

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "control_panel.h"
+#include "panel_info.h"
+#include "panel_serial.h"
+#include "panel_setup.h"
+#include "panel_about.h"
+#include "panel_detail.h"
+
+extern QStackedWidget *stack;
+extern control_panel *cp;
+extern panel_info *pnl_info;
+extern panel_setup *pnl_setup;
+extern panel_serial *pnl_serial;
+extern panel_detail *pnl_detail;
+extern panel_about *pnl_about;
+
+#endif // MAIN_H
